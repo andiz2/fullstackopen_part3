a16e7bab0c68f292d711b734bb73fc53c146c1ca
andiz2 <codeandi@gmail.com>
2023-06-06 16:18:47 +0300

    3.12

---
+++ 3_12_2/mongo.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+
+
+const password = process.argv[2]
+const name = process.argv[3]
+const phoneNumber = process.argv[4]
+
+const url =
+  `mongodb+srv://test_fullstack:${password}@cluster0.obatlyn.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  phoneNumber: String,
+  id: Number,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length===3) {
+  console.log("phonebook:")
+  Person.find({}).then(result => {
+  result.forEach(person => {
+    console.log(person.name + " " + person.phoneNumber)
+  })
+  mongoose.connection.close()
+  })
+
+} else {
+
+const person = new Person({
+  name: name,
+  phoneNumber: phoneNumber,
+  id: 12,
+})
+
+person.save().then(result => {
+  console.log(`added ${name} number ${phoneNumber} to phonebook`)
+  //console.log(process.argv[3])
+  //console.log(process.argv[4])
+  //console.log(process.argv.length)
+  mongoose.connection.close()
+})
+}
+
+//FETCHING OBJ FROM THE DB
+
+
--- MongoDB/Mongoose/mongo2.js
+++ MongoDB/Mongoose/mongo2.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://test:test123@cluster0.obatlyn.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://test:password@cluster0.obatlyn.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
